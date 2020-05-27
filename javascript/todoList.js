0afafb4abf55f452acb05a511a2407ee921236a2
Cecilia Lobo <cecilialobo92@gmail.com>
2020-05-27 15:25:26 +0200

    add todo files

---
+++ javascript/todoList.js
@@ -0,0 +1,14 @@
+const todos = []
+
+let input = prompt ("What would you like to do?")
+
+while (input !== "quit") {
+    if (input === "list") {
+        console.log (todos);
+    } else if (input === "new") {
+        const newTodo = prompt ("Enter new todo");
+        todos.push (newTodo);
+    }
+    input = prompt ("What would you like to do?");
+}
+console.log ("Ok, you quit the app.");
