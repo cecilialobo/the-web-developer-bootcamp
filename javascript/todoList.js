9b81059fee56cd4f7698247eb666da53fb919122
Cecilia Lobo <cecilialobo92@gmail.com>
2020-05-28 13:22:40 +0200

    add some changes to todoList files

--- javascript/todoList.js
+++ javascript/todoList.js
@@ -1,14 +1,37 @@
-const todos = []
+const todos = ["do the dishes", "do laundry"]
 
 let input = prompt ("What would you like to do?")
 
 while (input !== "quit") {
     if (input === "list") {
-        console.log (todos);
+        listTodos () 
     } else if (input === "new") {
-        const newTodo = prompt ("Enter new todo");
-        todos.push (newTodo);
+        addNewTodo ()
+    } else if (input === "delete") {
+        deleteTodo ()
     }
     input = prompt ("What would you like to do?");
 }
-console.log ("Ok, you quit the app.");
+console.log ("You quit the app.");
+
+// functions are separated so the while loop looks more organized
+
+function listTodos () {
+    console.log ("***********")
+        todos.forEach (function(todo, i){
+            console.log (i + ": " + todo);
+        });
+        console.log ("***********")
+}
+
+function addNewTodo () {
+    const newTodo = prompt ("Enter new todo");
+        todos.push (newTodo);
+        console.log ("added todo");
+}
+
+function deleteTodo () {
+    let index = prompt ("Enter index of todo to delete")
+        todos.splice (index, 1);
+        console.log ("Deleted todo");
+}
