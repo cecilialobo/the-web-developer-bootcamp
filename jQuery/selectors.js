b966af8aceca49a6b7ccd6d6bbe9f7ac24e8de4f
Cecilia Lobo <cecilialobo92@gmail.com>
2020-06-15 13:25:20 +0200

    add jQuery folder

---
+++ jQuery/selectors.js
@@ -0,0 +1,11 @@
+// Select all divs and give them a purple background
+$("div").css("background", "purple");
+
+// Select all divs with the highlight class and make them 200px wide 
+$(".highlight").css("width", "200px");
+
+// Select the div with id third and give it a orange border
+$("#third").css("border", "2px solid orange");
+
+// Select the first div and change it's font color to pink
+$("div:first-of-type").css("color", "pink");
